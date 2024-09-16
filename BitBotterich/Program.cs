22859ee3afd4a03d99e45a7d0c24160a21371618
Simon Ickler <simon@MNZ-MWS-13903.ubisoft.org>
2024-09-16 13:35:20 +0200

    Initial commit

---
+++ BitBotterich/Program.cs
@@ -0,0 +1,67 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using BitBotterich;
+using System.Text;
+
+public class Program
+{
+    private static IConfiguration _config;
+    private static IServiceProvider _services;
+
+    private static readonly DiscordSocketConfig _socketConfig = new()
+    {
+        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+        AlwaysDownloadUsers = true,
+        UseInteractionSnowflakeDate = false,
+    };
+
+    public static async Task Main(string[] args)
+    {
+        if (!File.Exists("./config.json"))
+        {
+            FileStream config = File.Create("./config.json");
+
+            string configContent =
+@"{
+    ""TOKEN"": """"
+}
+";
+            config.Write(new UTF8Encoding(true).GetBytes(configContent), 0, configContent.Length);
+            config.Close();
+
+            Console.WriteLine("Please add a Token in the config.json (" + config.Name + ")");
+            return;
+        }
+
+        _config = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile(path: "config.json")
+            .Build();
+
+        _services = new ServiceCollection()
+            .AddSingleton(_config)
+            .AddSingleton(_socketConfig)
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+            .AddSingleton<InteractionHandler>()
+            .BuildServiceProvider();
+
+        var client = _services.GetRequiredService<DiscordSocketClient>();
+
+        client.Log += LogAsync;
+
+        await _services.GetRequiredService<InteractionHandler>()
+            .InitializeAsync();
+
+        await client.LoginAsync(TokenType.Bot, _config["Token"]);
+        await client.StartAsync();
+
+        await Task.Delay(Timeout.Infinite);
+    }
+
+    private static async Task LogAsync(LogMessage message)
+        => Console.WriteLine(message.ToString());
+}
---
+++ BitBotterich/util/InfoModule.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitBotterich
+{
+    public class InfoModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        public InteractionService Commands { get; set; }
+        private InteractionHandler _handler;
+
+        public InfoModule(InteractionHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [SlashCommand("say", "Repeat the input")]
+        public async Task Say(string message)
+            => await RespondAsync(message);
+
+        [SlashCommand("ping", "Pings the bot and returns its latency.")]
+        public async Task GreetUserAsync()
+            => await RespondAsync(text: $":ping_pong: It took me {Context.Client.Latency}ms to respond to you!", ephemeral: true);
+    }
+}
