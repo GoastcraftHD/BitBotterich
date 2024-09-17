b19e6ee2acd467782360f9eb03d128da1b17f451
Simon Ickler <simon@MNZ-MWS-13903.ubisoft.org>
2024-09-17 11:02:28 +0200

    Added Zitat Command (No functionality)

---
+++ BitBotterich/commands/QuoteCommand.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitBotterich.commands
+{
+    [Discord.Interactions.Group("zitat", "Alle Commands für zitate.")]
+    public class QuoteCommand : InteractionModuleBase<SocketInteractionContext>
+    {
+        public InteractionService Commands { get; set; }
+        private InteractionHandler _handler;
+
+        public QuoteCommand(InteractionHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [SlashCommand("create", "Erstellt einen neuen Zitat Zähler.")]
+        public async Task Say(string zitat, string name)
+            => await RespondAsync(zitat + " : " + name);
+    }
+}
